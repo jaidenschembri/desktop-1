02d8b7973a2e58b5830cbb961846ef6b85221210
jaidenschembri <jaidenschembri1@gmail.com>
2025-04-17 23:16:53 -0700

    🚀 deploy

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import fs from 'fs';
+import path from 'path';
+
+const isProd = process.env.NODE_ENV === 'production';
+
+export default defineConfig({
+  base: isProd ? '/desktop/' : '/', // 👈 dynamic base path
+  server: {
+    open: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    copyPublicDir: true
+  },
+  publicDir: 'public',
+  plugins: [
+    {
+      name: 'add-nojekyll',
+      closeBundle() {
+        const noJekyllPath = path.resolve(__dirname, 'dist/.nojekyll');
+        fs.writeFileSync(noJekyllPath, '');
+        console.log('✅ .nojekyll file created in dist/');
+      }
+    }
+  ]
+});
